5a26770f1dc41d5ac4d99d4b09cd040137bdd336
Da Xue <da@lessconfused.com>
2015-11-14 23:40:57 -0500

    Flash 3

--- other/flash/flash.sh
+++ other/flash/flash.sh
@@ -26,56 +26,98 @@ fi
 
 IMG_FILE="2015-09-24-raspbian-jessie.img"
 
-IMG_SIZE=`stat -c %s "$IMG_FILE"`
-echo "$1:	Image Size:	$IMG_SIZE BYTES"
+IMG_SIZE_B=`stat -c %s "$IMG_FILE"`
+echo "$1:	Image Size:	$IMG_SIZE_B BYTES"
 
-DEV_SIZE=`blockdev --getsize64 "/dev/$1"`
+DEV_SIZE_B=`blockdev --getsize64 "/dev/$1"`
 if [ $? -ne 0 ]; then
 	echo "$1:	Unable to get blocksize."
 	exit 1
 fi
-echo "$1:	Device Size:	$DEV_SIZE BYTES"
+echo "$1:	Device Size:	$DEV_SIZE_B BYTES"
 
-if [ $IMG_SIZE -gt $DEV_SIZE ]; then
+if [ $IMG_SIZE_B -gt $DEV_SIZE_B ]; then
 	echo "$1:	Image size is larger than device size."
 	exit 1
 fi
 
-BS=1048576
-IMG_CNT=$((IMG_SIZE / BS + (IMG_SIZE % BS > 0)))
+function checkUSBPath {
+	local USB_PATH_NEW
+	USB_PATH_NEW=`readlink -f /sys/block/$1`
+	USB_PATH_NEW="${USB_PATH_NEW%%/host*}"
+	USB_PATH_NEW="${USB_PATH_NEW##*/}"
+	if [ -z "$USB_PATH" ]; then
+		USB_PATH="$USB_PATH_NEW"
+	elif [ "$USB_PATH_NEW" != "$USB_PATH" ]; then
+		echo "=====USB DEVICE CHANGED FROM $USB_PATH TO $USB_PATH_NEW====="
+		exit 1
+	fi
+}
+
+B_PER_MB=1048576
+BLK_MB=3
+BLK_B=$((BLK_MB * B_PER_MB))
+IMG_BLK_CNT=$((IMG_SIZE_B / BLK_B + (IMG_SIZE_B % BLK_B > 0)))
 BLOCKS_FIXED=0
 BLOCKS_FIXED_AGAIN=0
-echo "$1:	Reading:	$IMG_CNT MBYTES"
+BLOCKS_FIXED_CONT=0
+BLOCKS_FIXED_CONT_LIMIT=3
+BLOCKS_FIXED_CONT_LIMIT_REACHED=0
+echo "$1:	Reading:	$IMG_BLK_CNT BLOCKS OF $BLK_MB MBYTES"
 echo "0" > /sys/block/$1/queue/read_ahead_kb
-#set -x
-cmp -s -n $((BS*100)) $IMG_FILE /dev/$1
-if [ $? -ne 0 ]; then
-	echo "$1:	First 100MB does not match!"
-	dd if=$IMG_FILE of=/dev/$1 bs=1M oflag=direct 2> /dev/null
+echo "2048" > /sys/block/$1/device/max_sectors
+
+if [ "$DEBUG" = "1" ]; then
+	set -x
 fi
-for i in `seq 0 $IMG_CNT`; do
-	if [ $i -eq $IMG_CNT ]; then
-		cmp -s $IMG_FILE <(dd if=/dev/$1 bs=1M count=$IMG_CNT iflag=direct 2> /dev/null)
+
+cmp -s -n $BLK_B $IMG_FILE /dev/$1
+
+if [ $? -ne 0 ]; then
+	checkUSBPath
+	echo "$1:	First block does not match!"
+	dd if=$IMG_FILE of=/dev/$1 bs=$BLK_B oflag=direct 2> /dev/null
+fi
+for i in `seq 0 $IMG_BLK_CNT`; do
+	if [ $i -eq $IMG_BLK_CNT ]; then
+		cmp -s $IMG_FILE <(dd if=/dev/$1 bs=$BLK_B count=$IMG_BLK_CNT iflag=direct 2> /dev/null)
 		if [ $? -ne 0 ]; then
+			checkUSBPath
 			echo "$1:	CRITICAL!!! File does not match after check and rewrite! $BLOCKS_FIXED"
 			exit 1
 		fi
 		echo "$1:	File matches! $BLOCKS_FIXED"
 		exit 0
 	fi
-	cmp -s <(dd if=$IMG_FILE bs=1M skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=1M skip=$i count=1 iflag=direct 2> /dev/null)
+	cmp -s <(dd if=$IMG_FILE bs=$BLK_B skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=$BLK_B skip=$i count=1 iflag=direct 2> /dev/null)
 	if [ $? -ne 0 ]; then
-		echo -e -n "\r$1:	Mismatch at $i MB."
-		dd if=$IMG_FILE of=/dev/$1 bs=1M skip=$i seek=$i count=1 oflag=direct 2> /dev/null && cmp -s <(dd if=$IMG_FILE bs=1M skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=1M skip=$i count=1 iflag=direct 2> /dev/null)
-		if [ $? -ne 0 ]; then
-			echo "$1:	Unable to fix block $i!"
-			dd if=$IMG_FILE of=/dev/$1 bs=1M skip=$i seek=$i count=1 oflag=direct 2> /dev/null && cmp -s <(dd if=$IMG_FILE bs=1M skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=1M skip=$i count=1 iflag=direct 2> /dev/null)
+		checkUSBPath
+		((BLOCKS_FIXED_CONT++))
+		if [ "$BLOCKS_FIXED_CONT" -gt "$BLOCKS_FIXED_CONT_LIMIT" ]; then
+			((BLOCKS_FIXED_CONT_LIMIT_REACHED++))
+			echo "$1:	Reached continuous fixed block limit at $i. Flashing from $i to $IMG_BLK_CNT!"
+			dd if=$IMG_FILE of=/dev/$1 bs=$BLK_B skip=$i seek=$i count=$((IMG_BLK_CNT - i)) oflag=direct 2> /dev/null
 			if [ $? -ne 0 ]; then
+				checkUSBPath
+				echo "$1:	Flashing from $i to $IMG_BLK_CNT failed!"
+			fi
+			cmp -s <(dd if=$IMG_FILE bs=$BLK_B skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=$BLK_B skip=$i count=1 iflag=direct 2> /dev/null)
+		else
+			echo "$1:	Mismatch at block $i, $((i * BLK_MB)) MB."
+			dd if=$IMG_FILE of=/dev/$1 bs=$BLK_B skip=$i seek=$i count=1 oflag=direct 2> /dev/null && cmp -s <(dd if=$IMG_FILE bs=$BLK_B skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=$BLK_B skip=$i count=1 iflag=direct 2> /dev/null)
+		fi
+		if [ $? -ne 0 ]; then
+			checkUSBPath
+			echo "$1:	Unable to fix block $i!"
+			dd if=$IMG_FILE of=/dev/$1 bs=$BLK_B skip=$i seek=$i count=1 oflag=direct 2> /dev/null && cmp -s <(dd if=$IMG_FILE bs=$BLK_B skip=$i count=1 2> /dev/null) <(dd if=/dev/$1 bs=$BLK_B skip=$i count=1 iflag=direct 2> /dev/null)
+			if [ $? -ne 0 ]; then
+				checkUSBPath
 				echo "$1:	Unable to fix block $i again!"
 				exit 1
 			fi
-			((BLOCKS_FIXED_AGAIN++))
 		fi
 		((BLOCKS_FIXED++))
+	else
+		BLOCKS_FIXED_CONT=0
 	fi
 done
